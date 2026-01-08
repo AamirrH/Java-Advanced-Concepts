76b0c241d60cbc74a3c58d02d61e994a530393db
AamirrH <aamirr.1704@gmail.com>
2026-01-08 23:45:14 +0530

    Objects,Reference and Classes

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    // Object, Reference and Classes In-Depth Logic
+    Car carRef = new Car();
+    // carRef stores the reference to the Car Object created in heap memory
+    System.out.println(carRef); // Prints location of Car Object Car@8efb846
+    Car carRef2 = carRef;
+    System.out.println(carRef2); /* Prints location Car@8efb846 again,
+    carRef actually stores the reference of actual Car Object
+    created which looks something like Car@8efb846
+    when we assign carRef2 to carRef we are actually assigning,
+    carRef2 to the reference Patient@8efb846
+    hence carRef2 and carRef now refer to the same object.
+    */
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+
 }
